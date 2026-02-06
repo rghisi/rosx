86e8992a1f7980348cbbe17f433f28dff21df93d
Ricardo Ghisi Tobaldini <rghisi@gmail.com>
2026-02-06 23:10:56 +0100

    [Shell] Remove unnecessary delay function

--- apps/shell/src/shell.rs
+++ apps/shell/src/shell.rs
@@ -1,5 +1,4 @@
-use core::arch::asm;
-use usrlib::{print, println};
+use usrlib::println;
 use usrlib::syscall::Syscall;
 
 static mut COUNT: usize = 0;
@@ -12,12 +11,4 @@ pub fn main() {
     Syscall::exec(main as usize);
     Syscall::sleep(500);
     println!("\nclosing shell {}", c);
-}
-
-fn delay(ticks: u32) {
-    for _ in 0..ticks {
-        unsafe {
-            asm!("nop");
-        }
-    }
 }
