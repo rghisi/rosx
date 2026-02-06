e7c1fc0827bd3fd51d8f984f17d594a96114de2d
Ricardo Ghisi Tobaldini <rghisi@gmail.com>
2026-02-06 23:45:15 +0100

    [Shell] Basic buffering and "enter" handling

--- apps/shell/src/shell.rs
+++ apps/shell/src/shell.rs
@@ -1,14 +1,30 @@
 use usrlib::{print, println};
 use usrlib::syscall::Syscall;
+use alloc::string::String;
 
 pub fn main() {
     println!("RosX Shell");
     print!("> ");
+    
+    let mut buffer = String::new();
+    
     loop {
         let c = Syscall::read_char();
-        print!("{}", c);
+        
         if c == '\n' {
+            println!();
+            
+            if buffer == "ls" {
+                println!("Listing Files");
+            } else if !buffer.is_empty() {
+                println!("Unknown command: {}", buffer);
+            }
+            
+            buffer.clear();
             print!("> ");
+        } else {
+            print!("{}", c);
+            buffer.push(c);
         }
     }
 }
