70ee65d5611239695abb69cd36b4ad265e3bdede
Ricardo Ghisi Tobaldini <rghisi@gmail.com>
2026-02-10 23:06:16 +0100

    [Apps] Create hello_elf standalone PIE binary

---
+++ apps/hello_elf/src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::panic::PanicInfo;
+use usrlib::println;
+
+struct SyscallAllocator;
+
+unsafe impl GlobalAlloc for SyscallAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        usrlib::syscall::Syscall::alloc(layout.size(), layout.align())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        usrlib::syscall::Syscall::dealloc(ptr, layout.size(), layout.align());
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: SyscallAllocator = SyscallAllocator;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, World!");
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
