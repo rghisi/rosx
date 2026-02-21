e743b4bb528c466ce29f43a383edf5aaec84c1b9
Ricardo Ghisi Tobaldini <rghisi@gmail.com>
2026-02-21 19:32:28 +0100

    [Tetris] Add tetris app scaffold

---
+++ apps/tetris/src/main.rs
@@ -0,0 +1,34 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::panic::PanicInfo;
+use usrlib::{println};
+use usrlib::syscall::Syscall;
+
+struct SyscallAllocator;
+
+unsafe impl GlobalAlloc for SyscallAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        Syscall::alloc(layout.size(), layout.align())
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        Syscall::dealloc(ptr, layout.size(), layout.align());
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: SyscallAllocator = SyscallAllocator;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() {
+    println!("Tetris");
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
